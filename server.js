5f237fe5bac8044d6c67dd229a505d06a07155e7
sushantpDev <codesusi59@gmail.com>
2026-05-30 13:17:34 +0530

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+});
